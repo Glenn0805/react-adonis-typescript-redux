33467bd2944ad25222d053460b853df64685bf6b
tmjp-Glenn <glenn@tmjpbpo.com>
2022-04-01 10:29:03 +0800

    create actions

--- resources/js/main/component/foobar/foobar-actions.ts
+++ resources/js/main/component/foobar/foobar-actions.ts
@@ -1,6 +1,25 @@
+/*eslint-disable*/
 export const setModuleState = (payload) => async (dispatch) => {
   dispatch({
     type: 'SET_FOOBAR_STATE',
     payload,
   })
+}
+
+export const incrementFunction = (increment:number) => async (dispatch) => {
+  dispatch({
+    type: 'SET_FOOBAR_STATE',
+    payload: {
+        numberState: ++increment,
+        }
+  })
+}
+
+export const decrementFunction = (decrement:number) => async (dispatch) => {
+  dispatch({
+        type: 'SET_FOOBAR_STATE',
+        payload: {
+        numberState: --decrement,
+        }
+      })
 }
